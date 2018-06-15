75bd094591c3446c6232187ab118172827bd704d
Katherine Pangtay <kpangtay@gmail.com>
2018-06-15 15:12:39 -0400

    error

--- routes/wiki.js
+++ routes/wiki.js
@@ -1,14 +1,26 @@
 const express = require('express');
 const router = express.Router();
 const addPage = require('../views/addPage');
+const { Page } =require("../models")
 
 router.get('/', (req, res, next) => {
 	res.send('');
 });
 
-router.post('/', (req, res, next) => {
-	res.json(req.body);
+router.post('/', async (req, res, next) => {
+	const page = new Page({
+		title: req.body.title,
+		content: req.body.content
+	})
+	try{
+		await page.save()
+		res.redirect('/')
+	}catch(error){
+		next(error)
+	}
 });
+
+
 
 router.get('/add', (req, res, next) => {
 	res.send(addPage());
